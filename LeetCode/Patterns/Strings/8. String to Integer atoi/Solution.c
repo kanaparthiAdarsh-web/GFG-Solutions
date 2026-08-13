8a48526305d43f85e34119737f898c3c10e4c212
Adarsh Kanaparthi <kanaparthiadarsh@gmail.com>
2026-08-14 00:13:48 +0530

    Add C Solution to Patterns/Strings

--- LeetCode/Patterns/Strings/8. String to Integer atoi/Solution.c
+++ LeetCode/Patterns/Strings/8. String to Integer atoi/Solution.c
@@ -6,24 +6,22 @@ int myAtoi(char* s) {
     long long num = 0;
     int sign = 1;
     
-    while (s[i] == ' ') {
+    while (s[i] == ' ')
         i++;
-    }
-    
-    if (s[i] == '-') {
+    if (s[i] == '-')
+    {
         sign = -1;
         i++;
-    } else if (s[i] == '+') {
-        i++;
     }
-    
-    while (s[i] >= '0' && s[i] <= '9') {
+    else 
+        if (s[i] == '+')
+            i++;
+    while (s[i] >= '0' && s[i] <= '9')
+    {
         int digit = s[i] - '0';
         
-        if (num > (INT_MAX - digit) / 10) {
+        if (num > (INT_MAX - digit) / 10)
             return (sign == 1) ? INT_MAX : INT_MIN;
-        }
-        
         num = num * 10 + digit;
         i++;
     }
